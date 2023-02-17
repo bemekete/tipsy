2fb9593fcaebd37ad06d0654e4c473f62da79ef2
suji <wjrwowjrth@gmail.com>
2023-02-17 16:36:10 +0900

    디테일 페이지 수정

---
+++ detail/JS/detail.js
@@ -0,0 +1,41 @@
+// 상품 이름, 가격 저장
+const title = document.querySelector('.detail_tit');
+
+let titleTop = title.querySelector('.detail_tit_top span'),
+    price = title.querySelector('.price span');
+
+titleTop = titleTop.innerText;
+price = price.innerText.replace(',', '');
+
+
+// 플로팅바 수량에 따른 가격 검색 기능
+const floatBar = document.querySelector('.rightFloat'),
+    pieces = document.getElementById('pieces'),
+    [numDown, numUp] = floatBar.getElementsByClassName('numfor'),
+    calcPrice = floatBar.querySelector('.productPrice span');
+
+// 장바구니 팝업
+const basket = floatBar.querySelector('.popupBasket');
+
+floatBar.addEventListener('click', (e) => {
+    const target = e.target;
+
+    if (target === numDown) {
+        if (pieces.value < 1) return;
+        pieces.value = +pieces.value - 1;
+    }
+    if (target === numUp) {
+        if (pieces.value >= 20) return
+        pieces.value = +pieces.value + 1;
+    }
+    let calc = `${price * pieces.value}`;
+    calcPrice.innerText = Number(calc).toLocaleString("ko-KR");
+
+
+    if (target === floatBar.querySelector('.productBuy_btn button')) {
+        basket.style.display = 'block';
+    }
+    if (target === basket.querySelector('.popupClose')) {
+        basket.style.display = 'none';
+    }
+})
