a62c9c8e90b2b966ae3efd7ae87761520c0724e2
jihwan <guswlghks007@naver.com>
2023-02-21 15:08:46 +0900

    장바구니 페이지 임시완료

--- shopBasket/js/shopBasket.js
+++ shopBasket/js/shopBasket.js
@@ -18,7 +18,10 @@ const bodyHidden = document.querySelector('body'),
     goods_img = CartBody.querySelectorAll('.goods_img'),
     count_box_main = document.querySelector('.count_box_main'),
     count_box_dl = count_box_main.querySelector('dl'),
-    td_goods = CartBody.querySelectorAll('.td_goods');
+    td_goods = CartBody.querySelectorAll('.td_goods'),
+    goods_delete_btn = document.querySelector('.goods_delete_btn'),
+    no_data = document.querySelector('.no_data'),
+    txt = CartBody.querySelectorAll('.txt');
 
 
 const checkedInput = 'input:checked';
@@ -38,6 +41,22 @@ function headCheckbox(value) {
 function CheckedBlock() {
     const checkedElement = CartBody.querySelectorAll(checkedInput);
     checkedElementCnt = checkedElement.length;
+}
+
+// 상품 총 가격 함수
+function reloadSum() {
+    let sum, deli, laterSum = 0, laterDeli = 0;
+    for (let i = 0; i < CartBodyInput.length; i++) {
+        CartBodyInput[i].checked === true ? sum = txt[i].textContent.replace(',', '') : sum = 0;
+        CartBodyInput[i].checked === true ? deli = '3000' : deli = 0;
+        laterSum += Number(sum);
+        laterDeli += Number(deli);
+        if (laterDeli) laterDeli = '3,000';
+        price_sum_num[1].textContent = laterSum.toLocaleString();
+        price_sum_num[2].textContent = laterDeli;
+        price_sum_num[3].textContent = (Number(price_sum_num[1].textContent.replace(',', '')) + Number(price_sum_num[2].textContent.replace(',', ''))).toLocaleString();
+    }
+
 }
 
 // 합계금액 표시
@@ -62,13 +81,19 @@ CartHead.addEventListener('input', function () {
     allCheck.checked ? otherCheckbox(true) : otherCheckbox(false);
 });
 CartBody.addEventListener('input', function () {
-    CartBodyInput[0].checked && CartBodyInput[1] ? headCheckbox(true) : headCheckbox(false);
+    CartBodyInput[0].checked && CartBodyInput[1].checked ? headCheckbox(true) : headCheckbox(false);
 });
+
+table.addEventListener('input', function () {
+    reloadSum();
+});
+
 
 // 체크 박스 선택에 따른 가격 및 수량 변경 부분
 
 
 window.addEventListener('load', function () {
+    reloadSum();
     CheckedBlock();
     price_sum_num[0].textContent = checkedElementCnt;
     let sum = 0;
@@ -81,9 +106,6 @@ window.addEventListener('load', function () {
 table.addEventListener('input', function () {
     CheckedBlock();
     price_sum_num[0].textContent = checkedElementCnt;
-    if (CartBodyInput[0].checked === true) {
-
-    }
 
 });
 
@@ -92,7 +114,7 @@ table.addEventListener('input', function () {
 for (let i = 0; i < td_count_Pdt.length; i++) {
     td_count_Pdt[i].addEventListener('click', function (e) {
         e.preventDefault();
-        const orderBtn = e.target.closest('a');
+        let orderBtn = e.target.closest('a');
         if (this.contains(orderBtn)) {
             bodyHidden.classList.add('nonOverscroll');
             optionView.classList.remove('displayNone');
@@ -103,16 +125,30 @@ for (let i = 0; i < td_count_Pdt.length; i++) {
     });
 }
 
+//옵션 수량 변경 부분 (변경 창 전체)
 goods_count_box.addEventListener('click', function (e) {
     e.preventDefault();
-    const closeBtn = e.target.closest('span');
+    let closeBtn = e.target.closest('span');
     if (this.contains(closeBtn)) {
         bodyHidden.classList.remove('nonOverscroll');
         optionView.classList.add('displayNone');
     }
 });
 
+// 선택 상품 삭제
 
-//옵션 수량 변경 부분 (변경 창 전체)
-
+goods_delete_btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (confirm('정말 삭제하시겠습니까?')) {
+        for (let i = 0; i < CartBodyInput.length; i++) {
+            if (CartBodyInput[i].checked) {
+                CartBody.children[i].style.display = 'none';
+            }
+            if (allCheck.checked) {
+                CartHead.children[0].style.display = 'none';
+                no_data.classList.remove('displayNone');
+            }
+        }
+    }
+});
 
