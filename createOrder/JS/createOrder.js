924a4982b79a1ec6fba41f8bc7fd26fe94e4e037
seul8975 <seul8975@naver.com>
2023-02-27 19:27:12 +0900

    주문완료페이지 생성과 주문서 작성페이지 form 경로 수정, 및 짜잘한거 수정

--- createOrder/JS/createOrder.js
+++ createOrder/JS/createOrder.js
@@ -1,24 +1,4 @@
 'use strict';
-// header category
-
-const detailCategoryList = document.querySelector('.detailCategoryList'),
-    drinkhoverOf = detailCategoryList.querySelector('.detailCategoryListof');
-
-detailCategoryList.addEventListener('mouseover', (e) => {
-    const [drinkhover, snackhover] = detailCategoryList.getElementsByClassName('hoverCategory');
-
-    if (e.target === drinkhover) {
-        drinkhoverOf.classList.remove('hidden')
-    } else if (e.target === snackhover) {
-        drinkhoverOf.classList.add('hidden');
-    } else return;
-})
-
-detailCategoryList.addEventListener('mouseleave', () => {
-    drinkhoverOf.classList.remove('hidden')
-})
-
-
 
 let main = document.getElementById('main'),
     main_down = main.getElementsByClassName('main_down'),
@@ -33,23 +13,10 @@ main.addEventListener('click', (e) => {
         couponclickBtn.classList.toggle('main_none');
         // 버튼 방향 up, down
         targetEvent.classList.toggle('main_up');
+
     } else if (targetEvent === main_down[1]) {
         agreeText.classList.toggle('main_none');
         targetEvent.classList.toggle('main_up');
+
     } else return;
-});
-
-// 탑플로팅바
-const topfloatingbar = document.querySelector('.topfloatingbar');
-
-document.addEventListener('scroll', function () {
-    let posY = window.scrollY;
-
-    if (posY === 0) {
-        topfloatingbar.style.visibility = 'hidden';
-        topfloatingbar.style.opacity = '0';
-    } else if (posY > 10) {
-        topfloatingbar.style.visibility = 'visible';
-        topfloatingbar.style.opacity = '.3';
-    }
-});
+})
